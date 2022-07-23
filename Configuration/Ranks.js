bd2bba33634314ec425b0b53d4e785b8fbd5863f
$~Bunni <emily@bunni.me>
2022-07-23 14:29:14 -0400

    updated ranks

--- Configuration/Ranks.js
+++ Configuration/Ranks.js
@@ -6,24 +6,24 @@ module.exports = {
             color: 'DARK_RED',
             permissions: ['*'],
             italics: false,
-            bold: false,
+            bold: true,
             id: 'owner'
-        },
-        {
-            name: 'Developer',
-            color: 'BLUE',
-            permissions: ['*'],
-            italics: false,
-            bold: false,
-            id: 'developer'
         },
         {
             name: 'Platform Admin',
             color: 'DARK_RED',
             permissions: ['*'],
             italics: true,
-            bold: false,
+            bold: true,
             id: 'platadmin'
+        },
+        {
+            name: 'Developer',
+            color: 'BLUE',
+            permissions: ['*'],
+            italics: false,
+            bold: true,
+            id: 'developer'
         },
         {
             name: 'Senior Admin',
@@ -37,13 +37,13 @@ module.exports = {
             name: 'Admin',
             color: 'RED',
             permissions: ['ADMINISTRATION', 'TICKETS_ADMIN', 'STAFF'],
-            italics: false,
+            italics: true,
             bold: false,
             id: 'admin'
         },
         {
             name: 'Senior Mod',
-            color: 'CYAN',
+            color: 'DARK_PURPLE',
             permissions: ['TICKETS_MODERATE', 'STAFF'],
             italics: true,
             bold: false,
@@ -51,19 +51,19 @@ module.exports = {
         },
         {
             name: 'Moderator',
-            color: 'CYAN',
+            color: 'DARK_PURPLE',
             permissions: ['TICKETS_MODERATE', 'STAFF'],
-            italics: false,
+            italics: true,
             bold: false,
-            id: 'mod'
+            id: 'moderator'
         },
         {
-            name: 'Trainee',
+            name: 'Trial Mod',
             color: 'YELLOW',
             permissions: ['STAFF'],
-            italics: false,
+            italics: true,
             bold: false,
-            id: 'trainee'
+            id: 'trialmod'
         },
         // end of staff
         // Media Ranks
@@ -72,90 +72,66 @@ module.exports = {
             name: 'Partner',
             color: 'LIGHT_PURPLE',
             permissions: ['CREATOR'],
-            italics: false,
+            italics: true,
             bold: false,
             id: 'partner'
         },
         {
             name: 'Famous',
-            color: 'LIGHT_RED',
+            color: 'DARK_PURPLE',
             permissions: ['CREATOR'],
-            italics: false,
+            italics: true,
             bold: false,
             id: 'famous'
         },
         {
             name: 'Media',
-            color: 'DARK_PURPLE',
+            color: 'RED',
             permissions: ['CREATOR'],
-            italics: false,
+            italics: true,
             bold: false,
             id: 'media'
         },
         // end of media
         {
-            name: 'Miner',
-            color: 'YELLOW',
-            permissions: [],
-            italics: false,
-            bold: false,
-            id: 'miner'
-        },
-        {
-            name: 'Queen',
-            color: 'PURPLE',
-            permissions: ['ROYAL'],
-            italics: false,
-            bold: false,
-            id: 'queen'
-        },
-        {
-            name: 'King',
-            color: 'PURPLE',
-            permissions: ['ROYAL'],
-            italics: false,
-            bold: false,
-            id: 'king'
-        },
-        {
-            name: 'High Roller',
+            name: 'Beach',
             color: 'GOLD',
             permissions: [],
-            italics: false,
-            bold: false,
-            id: 'high roller'
+            italics: true,
+            bold: true,
+            id: 'beach'
         },
         {
-            name: 'Epic',
+            name: 'Ocean',
             color: 'BLUE',
             permissions: [],
             italics: false,
             bold: false,
-            id: 'epic'
+            id: 'ocean'
         },
         {
-            name: 'PRO',
-            color: 'GOLD',
-            permissions: [],
-            italics: false,
-            bold: false,
-            id: 'pro'
-        },
-        {
-            name: 'MVP',
-            color: 'AQUA',
-            permissions: [],
-            italics: false,
-            bold: false,
-            id: 'mvp'
-        },
-        {
-            name: 'VIP',
+            name: 'Squid',
             color: 'GREEN',
             permissions: [],
             italics: false,
             bold: false,
-            id: 'vip'
+            id: 'squid'
+        },
+        {
+            name: 'Shark',
+            color: 'DARK_GREEN',
+            permissions: [],
+            italics: false,
+            bold: false,
+            id: 'shark'
+        },
+        {
+            name: 'Fish',
+            color: 'YELLOW',
+            permissions: [],
+            italics: false,
+            bold: false,
+            id: 'fish'
         },
         {
             name: 'Default',
